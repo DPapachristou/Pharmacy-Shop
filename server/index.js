4fe8d928ccdb47fa66b35bbae591c99e33e7f109
DPapachristou <d.k.papachristou@gmail.com>
2023-07-29 20:42:10 +0300

    Server Upadate 2 - Authentication

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/users");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -10,7 +12,11 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("db connection success"));
 
-app.use("/server/user", userRoute);
+app.use(cors())
+app.use(express.json());
+app.use("/server/users", userRoute);
+app.use("/server/auth", authRoute);
+
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
--- server/routes/users.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-router.get("/usertest", (req,res)=>{
-    res.send("user test is succesfull");
-})
-
-
-
-module.exports = router;
